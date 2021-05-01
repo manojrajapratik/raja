25d0c4dcf29161d0bc248981cf3e3ea5131830e2
Raja <Raja@Sony>
2021-05-01 21:20:07 +0530

    test verify

---
+++ src/test/java/com/test/MavenActitime/TestVerify.java
@@ -0,0 +1,14 @@
+package com.test.MavenActitime;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestVerify {
+	
+	@Test
+	public void verify(){
+		
+		Reporter.log("new test",true);
+	}
+
+}
